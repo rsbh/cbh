884a9e2f42698c3a8083887e811101d3a58fd19b
Rishabh Mishra <me@rsbh.dev>
2023-01-23 15:00:59 +0530

    refactor: split pdeterministicPartitionKey function

--- dpk.js
+++ dpk.js
@@ -4,28 +4,22 @@ const createHash = (value) => {
   return crypto.createHash("sha3-512").update(value).digest("hex");
 }
 
+const getEventPartitionKey = (key,) => {
+  return typeof key === "string" ? key : JSON.stringify(key);
+}
+
+const createEventHashKey = (event) => {
+  const data = JSON.stringify(event);
+  return createHash(data)
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
   if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = createHash(data)
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    const partitionKey = event.partitionKey ? getEventPartitionKey(event.partitionKey, MAX_PARTITION_KEY_LENGTH) : createEventHashKey(event);
+    return partitionKey.length > MAX_PARTITION_KEY_LENGTH ? createHash(partitionKey) : partitionKey
   }
-
-
-  if (typeof candidate !== "string") {
-    candidate = JSON.stringify(candidate);
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = createHash(candidate)
-  }
-  return candidate;
+  return TRIVIAL_PARTITION_KEY;
 };
