9cc189cdb520bd906f4d2d30b076730f615b4311
Rishabh Mishra <me@rsbh.dev>
2023-01-23 14:46:06 +0530

    refactor: create createHash function

--- dpk.js
+++ dpk.js
@@ -1,5 +1,9 @@
 const crypto = require("crypto");
 
+const createHash = (value) => {
+  return crypto.createHash("sha3-512").update(value).digest("hex");
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
@@ -10,19 +14,18 @@ exports.deterministicPartitionKey = (event) => {
       candidate = event.partitionKey;
     } else {
       const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+      candidate = createHash(data)
     }
   } else {
     candidate = TRIVIAL_PARTITION_KEY;
   }
+
+
+  if (typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = createHash(candidate)
   }
   return candidate;
 };
