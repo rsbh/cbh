88d7d1cf2bcb46e11cefccf4220d7b0d11e40268
Rishabh Mishra <me@rsbh.dev>
2023-01-23 14:29:08 +0530

    tests: add test for dpk function

--- dpk.test.js
+++ dpk.test.js
@@ -5,4 +5,33 @@ describe("deterministicPartitionKey", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  it("should not returns the literal '0' when input is given", () => {
+    const event = { name: "Start" }
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).not.toBe("0");
+  });
+
+  it("should return `partitionKey` when `partitionKey` is there in event", () => {
+    const event = { name: "Start", partitionKey: "test_key" }
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("test_key");
+  });
+
+  it("should stringify the `partitionKey` when `partitionKey` is not string", () => {
+    const event = { name: "Start", partitionKey: ["test_key"] }
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey).toBe("[\"test_key\"]");
+    expect(typeof trivialKey).toBe("string");
+
+  });
+
+  it("should not return partition key with more than 256 length", () => {
+    const dummy_key = new Array(300).join("a")
+    const event = { name: "Start", partitionKey: dummy_key }
+    const trivialKey = deterministicPartitionKey(event);
+    expect(trivialKey.length).toBeLessThanOrEqual(256)
+  });
 });
+
+
